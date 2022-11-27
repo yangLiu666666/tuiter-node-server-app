c1842e88094a33818b8a5733fa8c5af58171ae9f
liuyang <yang.liu2@northeastern.edu>
2022-11-26 22:10:25 -0500

    add a9

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ import cors from 'cors';
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+//     || 'mongodb://localhost:27017/tuiter';
+// mongoose.connect(CONNECTION_STRING);
+mongoose.connect('mongodb://localhost:27017/tuiter');
+//mongoose.connect('mongodb+srv://liu:<password>@cluster0.inrvxch.mongodb.net/?retryWrites=true&w=majority');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
