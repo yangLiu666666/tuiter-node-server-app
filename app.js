f3843800c306a1729028269702482e575d260373
liuyang <yang.liu2@northeastern.edu>
2022-12-11 23:09:14 -0500

    fix heroku

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
+const app = express();
 await dotenv.config()
 // const CONNECTION_STRING = "mongodb+srv://liu:secretpassword@cluster0.inrvxch.mongodb.net/?retryWrites=true&w=majority";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
     || 'mongodb://localhost:27017/tuiter'
 mongoose.connect(CONNECTION_STRING);
 
-const app = express();
 app.use(cors());
 app.use(express.json());
 TuitsController(app);
